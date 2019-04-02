a421d41a17c5ef28e5da1dd72e882d71e880f862
lua <luafanti@gmail.com>
2019-04-02 12:55:40 +0200

    Update outgoes

--- functions/outgo.js
+++ functions/outgo.js
@@ -9,6 +9,7 @@ import * as dynamoDbLib from "../libs/dynamoDb-lib";
 
 
 const USERS_OUTGO_TABLE = process.env.USERS_OUTGO_TABLE;
+const USERS_CATEGORY_TABLE = process.env.USERS_CATEGORY_TABLE;
 const USER_NAME = process.env.USER_NAME;
 AWS.config.update({ region: "eu-west-1" });
 
@@ -38,35 +39,87 @@ app.get('/outgo/',async function (req, res) {
 app.post('/outgo', async function (req, res) {
     const { categoryId, description,value,date,place } = req.body;
 
-    if(!value) {
-        res.status(400).json({error: '"Field value is obligatory'});
-    }
-    if(!description) {
-        res.status(400).json({error: '"Field description is obligatory'});
-    }
 
+    if(!value) {
+        res.status(400).json({error: 'Field value is obligatory'});
+    }else if(!description) {
+        res.status(400).json({error: 'Field description is obligatory'});
+    }else if( ! await checkCategoryExist(categoryId)){
+        res.status(400).json({error: `Category ${categoryId} not exist`});
+    }else {
+
+        const params = {
+            TableName: USERS_OUTGO_TABLE,
+            Item: {
+                Username: USER_NAME,
+                OutgoId: uuid.v1(),
+                CategoryId: categoryId,
+                Value: value,
+                Description: description,
+                Place: place,
+                Date: date,
+            },
+        };
+        try{
+            await dynamoDbLib.call("put", params);
+            const {OutgoId} = params.Item;
+            res.json({ OutgoId }).status(201);
+        }catch (e) {
+            console.log(e);
+            res.status(400).json({ error: 'Could not create outgo' });
+        }
+    }
+})
+
+app.put('/outgo/:outgoId', async  function (req,res) {
+
+    const { categoryId, description,value,date,place } = req.body;
 
     const params = {
         TableName: USERS_OUTGO_TABLE,
-        Item: {
+        Key: {
             Username: USER_NAME,
-            OutgoId: uuid.v1(),
-            CategoryId: categoryId,
-            Value: value,
-            Description: description,
-            Place: place,
-            Date: date,
+            OutgoId : req.params.outgoId,
         },
+        UpdateExpression : 'set #cId = :CategoryId, #val = :Value, #desc = :Description, #pl = :Place, #date = :Date',
+        ExpressionAttributeNames: { '#cId' : 'CategoryId', '#val' : 'Value', '#desc' : 'Description', '#pl' : 'Place', '#date' : 'Date'},
+        ExpressionAttributeValues : { ':CategoryId' : categoryId, ':Value' : value, ':Description' : description, ':Place' : place, ':Date' : date},
+        ReturnValues: "ALL_NEW"
     };
+
     try{
-        await dynamoDbLib.call("put", params);
-        const {OutgoId} = params.Item;
-        res.json({ OutgoId }).status(201);
+        const result = await dynamoDbLib.call("update", params);
+        console.log(`Update outgo OK,  result = ${JSON.stringify(result)}`)
+        res.json(result.Attributes).status(200);
     }catch (e) {
         console.log(e);
-        res.status(400).json({ error: 'Could not create outgo' });
+        res.status(400).json({ error: 'Could not update outgo' });
     }
 
 })
 
+
+async function checkCategoryExist (categoryId)  {
+
+    const params = {
+        TableName: USERS_CATEGORY_TABLE,
+        Key: {
+            Username: USER_NAME,
+            CategoryId : categoryId,
+        }
+    };
+    try{
+        const result = await dynamoDbLib.call("get", params);
+        if(result.Item !== undefined && result.Item !== null){
+            console.log(`Category with Id [${categoryId}] exist`)
+            return true
+        }else{
+            console.log(`Category with Id [${categoryId}] not exist`)
+            return false
+        }
+    }catch (e) {
+        console.log(`Error during searching for category ${e}`)
+        return false
+    }
+}
 module.exports.handler = serverless(app);
